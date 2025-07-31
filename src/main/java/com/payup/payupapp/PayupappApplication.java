8b3fba9e6151da1a48e8f2d45a10f7b91dfe4092
Anurag Gupta <anuraggupta29@outlook.com>
2025-07-31 23:49:13 +0530

    Added base code

---
+++ src/main/java/com/payup/payupapp/PayupappApplication.java
@@ -0,0 +1,13 @@
+package com.payup.payupapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayupappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayupappApplication.class, args);
+	}
+
+}
